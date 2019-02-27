b7d7931ac1fb59870c8d31f83e48f91d73a3ee6c
Usuário Convidado <Guest@pa06micro09.local>
2019-02-27 08:21:28 -0300

    Initial Commit

---
+++ Exercicio/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exercicio
+//
+//  Created by Usuário Convidado on 27/02/19.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
